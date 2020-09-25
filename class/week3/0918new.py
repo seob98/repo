a3e54977a7112820d9eff82785b4813cf9c4aac4
seob98 <yongsoub9809@gmail.com>
2020-09-26 00:21:29 +0900

    수정

---
+++ class/week3/0918new.py
@@ -0,0 +1,32 @@
+from pico2d import *
+import os
+
+os.chdir('E:/python/0914')
+
+open_canvas()
+
+gra = load_image('../res/grass.png')
+ch = load_image('../res/run_animation.png')
+
+x = 0
+frame_index = 0
+while x < 800:
+    clear_canvas()
+    gra.draw(400,30)
+    ch.clip_draw(sx,0,100,100,x,85)
+    update_canvas()
+    
+
+    evts = get_events()
+
+    x += 2
+
+    frame_index = (frame_index + 1) % 8
+    
+    delay(0.01)
+
+    
+
+
+
+close_canvas()
